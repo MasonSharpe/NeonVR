0962232d2299e245bc9347736ffbb500c0b99dcd
Mason <115174711+MasonSharpe@users.noreply.github.com>
2023-11-16 13:10:10 -0800

    levl121!

---
+++ Assets/Scripts/AttachBallToPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachBallToPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject ball;
+    public bool isAttached = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isAttached = !isAttached;
+        }
+        if (isAttached) ball.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1);
+    }
+}
