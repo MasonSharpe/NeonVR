404ea49fa440a6dd9a720cfd046c50167ddef737
Mason <115174711+MasonSharpe@users.noreply.github.com>
2023-12-13 12:12:42 -0800

    treees!!!!

--- Assets/Scripts/AnimateHandOnInput.cs
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -18,8 +18,8 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
+        //float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        //handAnimator.SetFloat("Trigger", triggerValue);
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
